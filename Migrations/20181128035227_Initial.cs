8ff5bf76e2b9e46f0bc8fbc1303497a35b089244
Atsushi Suzuki <suzuki@exwzd.com>
2018-11-28 13:06:11 +0900

    initial commit

---
+++ Migrations/20181128035227_Initial.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace dotnet_ef_migrations_test.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "other");
+
+            migrationBuilder.CreateTable(
+                name: "Model",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Value1 = table.Column<string>(nullable: true),
+                    Value2 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Model", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Model",
+                schema: "other",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Value2 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Model", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Model_Model_Id",
+                        column: x => x.Id,
+                        principalTable: "Model",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Model",
+                schema: "other");
+
+            migrationBuilder.DropTable(
+                name: "Model");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    [Table("Model")]
+    public class Model1
+    {
+        public int Id { get; set; }
+        public string Value1 { get; set; }
+    }
+
+    [Table("Model", Schema = "other")]
+    public class Model2
+    {
+        public int Id { get; set; }
+        public string Value2 { get; set; }
+        public Model1 Model1 { get; set; }
+    }
+
+    public class AppContext : DbContext
+    {
+        public DbSet<Model2> Models { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+
+            builder.UseNpgsql("Host=localhost; Database=test; Username=postgres;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Model2>()
+                .HasOne(m => m.Model1)
+                .WithMany()
+                .HasForeignKey(m => m.Id);
+        }
+    }
+}
